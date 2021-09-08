8b01b2ee8d71fc68d67c802b154fc003c55d4eb9
JoaoDomiciano <becao97@gmail.com>
2021-09-08 09:35:17 -0300

    Class Cliente

---
+++ myBank/Cliente.cs
@@ -0,0 +1,6 @@
+public class Cliente{
+    public string titular;
+    public int agencia;
+    public int numero;
+    public double saldo;
+}
---
+++ myBank/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace myBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente contaDoJoao = new Cliente();
+            contaDoJoao.titular = "Joao";
+            contaDoJoao.agencia = 752;
+            contaDoJoao.numero = 752672;
+            contaDoJoao.saldo = 120;
+
+            Cliente contaDaBarbara = new Cliente();
+            contaDaBarbara.titular = "Barbara";
+            contaDaBarbara.agencia = 863;
+            contaDaBarbara.numero = 863146;
+            contaDaBarbara.saldo = 100;
+        }
+    }
+}
