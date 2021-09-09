514e79b6972b6fa21d5e1d614adb4ad023c7e6e5
JoaoDomiciano <becao97@gmail.com>
2021-09-09 09:35:30 -0300

    Constructor

--- myBank/Cliente.cs
+++ myBank/Cliente.cs
@@ -3,4 +3,13 @@ public class Cliente{
     public int agencia;
     public int numero;
     public double saldo;
+
+    public Cliente () {};
+    public Cliente (string titular, int agencia, int numero, double saldo)
+    {
+        this.titular = titular;
+        this.agencia = agencia;
+        this.numero = numero;
+        this.saldo = saldo;
+    }
 }
--- myBank/Program.cs
+++ myBank/Program.cs
@@ -6,17 +6,10 @@ namespace myBank
     {
         static void Main(string[] args)
         {
-            Cliente contaDoJoao = new Cliente();
-            contaDoJoao.titular = "Joao";
-            contaDoJoao.agencia = 752;
-            contaDoJoao.numero = 752672;
-            contaDoJoao.saldo = 120;
+            Cliente contaDoJoao = new Cliente("Joao", 752, 752672, 120);
+            Cliente contaDaBarbara = new Cliente("Barbara", 863, 863146, 100);
 
-            Cliente contaDaBarbara = new Cliente();
-            contaDaBarbara.titular = "Barbara";
-            contaDaBarbara.agencia = 863;
-            contaDaBarbara.numero = 863146;
-            contaDaBarbara.saldo = 100;
+            Console.WriteLine(contaDoJoao.titular);
         }
     }
 }
