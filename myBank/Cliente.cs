d1519d49b5c6a47023f9c2aab1c655fda49bcb58
JoaoDomiciano <becao97@gmail.com>
2021-09-15 14:02:50 -0300

    Opercoes de deposito e transferencia

--- myBank/Cliente.cs
+++ myBank/Cliente.cs
@@ -1,15 +1,48 @@
 public class Cliente{
-    public string titular;
+    
+    public string titular; 
     public int agencia;
-    public int numero;
+    public int conta; 
     public double saldo;
 
-    public Cliente () {};
-    public Cliente (string titular, int agencia, int numero, double saldo)
-    {
-        this.titular = titular;
-        this.agencia = agencia;
-        this.numero = numero;
-        this.saldo = saldo;
+    public Cliente() {}
+
+    public Cliente(string cliente_titular, int cliente_agencia, int cliente_conta, double cliente_saldo)
+    { 
+        titular = cliente_titular;
+        agencia = cliente_agencia;
+        conta = cliente_conta;
+        saldo = cliente_saldo;
     }
+
+    public bool Sacar (double valor)
+    {
+        if(this.saldo < valor){
+            return false;
+        }
+        else{
+            this.saldo -= valor;
+            return true;
+        }
+    }
+
+    public void Depositar (double valor)
+    {
+        this.saldo += valor;
+    }
+
+    public bool Transferir (double valor, Cliente contaDestino)
+    {
+        if (this.saldo < valor)
+        {
+            return false;
+        }
+        else
+        {
+            this.Sacar(valor);
+            contaDestino.Depositar(valor);
+            return true;
+        }
+    }
+
 }
--- myBank/Program.cs
+++ myBank/Program.cs
@@ -8,8 +8,24 @@ namespace myBank
         {
             Cliente contaDoJoao = new Cliente("Joao", 752, 752672, 120);
             Cliente contaDaBarbara = new Cliente("Barbara", 863, 863146, 100);
+            Cliente contaDaMaria = new Cliente("Maria", 123, 267254, 150);
 
-            Console.WriteLine(contaDoJoao.titular);
+            Console.WriteLine("O titular da conta é: " + contaDaBarbara.titular);
+            Console.WriteLine("O titular da conta é: " + contaDoJoao.titular);
+            Console.WriteLine("O titular da conta é: " + contaDaMaria.titular);
+
+            contaDaBarbara.Sacar(50);
+            contaDaMaria.Sacar(20);
+
+            Console.WriteLine("O saldo da conta da Barbara é: " + contaDaBarbara.saldo);
+            Console.WriteLine("O saldo da conta da Maria é: " + contaDaMaria.saldo);
+
+            contaDaBarbara.Depositar(20);
+            Console.WriteLine("O saldo da conta da Barbara é: " + contaDaBarbara.saldo);
+
+            contaDaBarbara.Transferir(100, contaDoJoao);
+            Console.WriteLine("O saldo da conta da Barbara é: " + contaDaBarbara.saldo);
+            Console.WriteLine("O saldo da conta do João é: " + contaDoJoao.saldo);
         }
     }
 }
