d99762a5d5d4284cad7d3b8a64b6793d64af1a82
JoaoDomiciano <becao97@gmail.com>
2021-09-23 09:40:14 -0300

    Desafios

--- myBank/Cliente.cs
+++ myBank/Cliente.cs
@@ -1,48 +1,55 @@
 public class Cliente{
     
-    public string titular; 
-    public int agencia;
-    public int conta; 
-    public double saldo;
+    public string Titular { get;set; }
+    public int Agencia { get;set; }
+    public int Conta { get;set; } 
 
-    public Cliente() {}
+    public static int TotalDeContasCriadas { get;set; }
+    private double _saldo;
+    public double Saldo {
+        get{
+            return this._saldo;
+        }
+        set{
+            if (value >= 0){
+                this._saldo = value;
+            }
+        }
+    }
+     public Cliente() {}
 
-    public Cliente(string cliente_titular, int cliente_agencia, int cliente_conta, double cliente_saldo)
+    public Cliente(string cliente_titular, int cliente_agencia, double cliente_saldo)
     { 
-        titular = cliente_titular;
-        agencia = cliente_agencia;
-        conta = cliente_conta;
-        saldo = cliente_saldo;
+        Titular = cliente_titular;
+        Agencia = cliente_agencia;
+        Saldo = cliente_saldo;
+
+        TotalDeContasCriadas ++;
     }
 
     public bool Sacar (double valor)
     {
-        if(this.saldo < valor){
+        if(this.Saldo < valor){
             return false;
         }
         else{
-            this.saldo -= valor;
+            this.Saldo -= valor;
             return true;
         }
     }
 
     public void Depositar (double valor)
     {
-        this.saldo += valor;
+        this.Saldo += valor;
     }
 
-    public bool Transferir (double valor, Cliente contaDestino)
-    {
-        if (this.saldo < valor)
-        {
+    public bool Transferir (double valor, Cliente contaDestino){
+        if (this.Saldo < valor){
             return false;
-        }
-        else
-        {
+        }else{
             this.Sacar(valor);
             contaDestino.Depositar(valor);
             return true;
         }
     }
-
 }
--- myBank/Program.cs
+++ myBank/Program.cs
@@ -6,26 +6,16 @@ namespace myBank
     {
         static void Main(string[] args)
         {
-            Cliente contaDoJoao = new Cliente("Joao", 752, 752672, 120);
-            Cliente contaDaBarbara = new Cliente("Barbara", 863, 863146, 100);
-            Cliente contaDaMaria = new Cliente("Maria", 123, 267254, 150);
+            Cliente contaDoJoao = new Cliente("Joao", 752, 120);
+            Cliente contaDaBarbara = new Cliente("Barbara", 863, 100);
+            Cliente contaDaMaria = new Cliente("Maria", 123, 150);
 
-            Console.WriteLine("O titular da conta é: " + contaDaBarbara.titular);
-            Console.WriteLine("O titular da conta é: " + contaDoJoao.titular);
-            Console.WriteLine("O titular da conta é: " + contaDaMaria.titular);
+            Console.WriteLine("O número da conta da Barbara é: " + contaDaBarbara.Conta);
+            Console.WriteLine("O saldo da conta da Barbara é: " + contaDaBarbara.Saldo);
+            Console.WriteLine("O número da conta da Joao é: " + contaDoJoao.Conta);
+            Console.WriteLine("O saldo da conta do Joao é: " + contaDoJoao.Saldo);
 
-            contaDaBarbara.Sacar(50);
-            contaDaMaria.Sacar(20);
-
-            Console.WriteLine("O saldo da conta da Barbara é: " + contaDaBarbara.saldo);
-            Console.WriteLine("O saldo da conta da Maria é: " + contaDaMaria.saldo);
-
-            contaDaBarbara.Depositar(20);
-            Console.WriteLine("O saldo da conta da Barbara é: " + contaDaBarbara.saldo);
-
-            contaDaBarbara.Transferir(100, contaDoJoao);
-            Console.WriteLine("O saldo da conta da Barbara é: " + contaDaBarbara.saldo);
-            Console.WriteLine("O saldo da conta do João é: " + contaDoJoao.saldo);
+            Console.WriteLine("O saldo da conta do Joao é: " + Cliente.TotalDeContasCriadas);
         }
     }
 }
